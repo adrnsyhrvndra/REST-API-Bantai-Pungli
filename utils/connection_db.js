25a0e52955141a613b481cc6d319cf6a6c4a4e51
Adriansyah Ravindra <adrimedia.indonesia@gmail.com>
2024-04-02 21:09:44 +0700

    2.2 Manage Connection Mongo DB Atlas & Vercell

--- utils/connection_db.js
+++ utils/connection_db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv').config();
 
-mongoose.connect(`mongodb://127.0.0.1:27017/pungli_db`).then(() => {
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}/${process.env.MONGO_DATABASE}?retryWrites=true&w=majority`).then(() => {
       console.log('MongoDB connected');
 }).catch((err) => {
       console.log('MongoDB connection error: ' + err);
