e6c3bfe8c648432ab0bca664833d3ccc83517292
Adriansyah Ravindra <adrimedia.indonesia@gmail.com>
2024-04-02 22:56:33 +0700

    2.9 Change DB Atlas Provider

--- utils/connection_db.js
+++ utils/connection_db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv').config();
 
-mongoose.connect(`mongodb+srv://adrimediawebdevindonesia:UrF0mMB2ZDGL6auI@bantaipungli.fxwzpyi.mongodb.net/db_pungli?retryWrites=true&w=majority`).then(() => {
+mongoose.connect(`mongodb+srv://adrimediawebdevindonesia:ynrt!e_WyC3F_vv@bantaipunglimongo.ag0xudb.mongodb.net/db_pungli?retryWrites=true&w=majority`).then(() => {
       console.log('MongoDB connected');
 }).catch((err) => {
       console.log('MongoDB connection error: ' + err);
