126d2e0bd5dda2a5928b905e124b7300b9e607c4
Adriansyah Ravindra <adrimedia.indonesia@gmail.com>
2024-04-03 13:04:31 +0700

    4.2 Admin Routes Add All Try Catch

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -17,21 +17,74 @@ router.use((req, res, next) => {
 
 
 router.get('/', async (req, res) => {
-      const admin = await AdminSchema.Admin.find();
-      res.json(admin);
+      try {
+            const admin = await AdminSchema.Admin.find();
+            res.json(admin);
+
+      } catch (error) {
+            res.status(500).json({
+                  success: false,
+                  message: error.message
+            });
+      }
 });
 
 router.get('/:id', async (req, res) => {
-      const admin = await AdminSchema.Admin.findById(req.params.id);
-      res.json(admin);
+      try {
+            const admin = await AdminSchema.Admin.findById(req.params.id);
+            res.json(admin);
+            
+      } catch (error) {
+            res.status(500).json({
+                  success: false,
+                  message: error.message
+            });
+      }
 });
 
 router.post('/', upload.single('foto_profile'), async (req, res) => {
-      const { username, password, email, nama_lengkap, tanggal_lahir, jenis_kelamin, alamat, no_telp, status_online, foto_profile} = req.body;
+      try {
+            const { username, password, email, nama_lengkap, tanggal_lahir, jenis_kelamin, alamat, no_telp, status_online, foto_profile} = req.body;
 
-      if (req.file) {
+            if (req.file) {
+                  
+                  cloudinary.uploader.upload(req.file.path, async (err, result) => {
+                        const adminData = await new AdminSchema.Admin({
+                              username,
+                              password,
+                              email,
+                              nama_lengkap,
+                              tanggal_lahir,
+                              jenis_kelamin,
+                              alamat,
+                              no_telp,
+                              foto_profile: result.url,
+                              status_online,
+                              created_at: new Date(),
+                              updated_at: new Date()
+                        });
+                  
+                        const data_keberhasilan_admin = await adminData.save();
+      
+                        if(err) {
+                              console.log(err);
+                              return res.status(500).json({
+                                    success: false,
+                                    message: "Error"
+                              })
+                        }
             
-            cloudinary.uploader.upload(req.file.path, async (err, result) => {
+                        res.status(200).json({
+                              success: true,
+                              message: "Success",
+                              data_profile: result,
+                              data: data_keberhasilan_admin
+                        });
+      
+                  });
+      
+            } else {
+      
                   const adminData = await new AdminSchema.Admin({
                         username,
                         password,
@@ -41,65 +94,75 @@ router.post('/', upload.single('foto_profile'), async (req, res) => {
                         jenis_kelamin,
                         alamat,
                         no_telp,
-                        foto_profile: result.url,
+                        foto_profile: null,
                         status_online,
                         created_at: new Date(),
                         updated_at: new Date()
                   });
-            
-                  adminData.save();
-
-                  if(err) {
-                        console.log(err);
-                        return res.status(500).json({
-                              success: false,
-                              message: "Error"
-                        })
-                  }
+      
+                  const data_keberhasilan_admin = await adminData.save();
       
                   res.status(200).json({
                         success: true,
                         message: "Success",
-                        data_profile: result,
-                        data: adminData
+                        data: data_keberhasilan_admin
                   });
+            }
 
-            });
-
-      } else {
-
-            const adminData = await new AdminSchema.Admin({
-                  username,
-                  password,
-                  email,
-                  nama_lengkap,
-                  tanggal_lahir,
-                  jenis_kelamin,
-                  alamat,
-                  no_telp,
-                  foto_profile: null,
-                  status_online,
-                  created_at: new Date(),
-                  updated_at: new Date()
-            });
-
-            adminData.save();
-
-            res.status(200).json({
-                  success: true,
-                  message: "Success",
-                  data: adminData
+      } catch (error) {
+            res.status(500).json({
+                  success: false,
+                  message: error.message
             });
       }
 });
 
 router.put('/:id', upload.single('foto_profile'), async (req,res) => {
-      const { username, password, email, nama_lengkap, tanggal_lahir, jenis_kelamin, alamat, no_telp, foto_profile, status_online } = req.body;
+      try {
+            const { username, password, email, nama_lengkap, tanggal_lahir, jenis_kelamin, alamat, no_telp, foto_profile, status_online } = req.body;
 
-      if (req.file) {
-
-            cloudinary.uploader.upload(req.file.path, async (err, result) => {
-
+            if (req.file) {
+      
+                  cloudinary.uploader.upload(req.file.path, async (err, result) => {
+      
+                        const adminUpdate = await AdminSchema.Admin.findOneAndUpdate(
+                              {_id: req.params.id},
+                              {
+                                    $set : {
+                                          username,
+                                          password,
+                                          email,
+                                          nama_lengkap,
+                                          tanggal_lahir,
+                                          jenis_kelamin,
+                                          alamat,
+                                          no_telp,
+                                          foto_profile: result.url,
+                                          status_online,
+                                          updated_at: new Date()
+                                    }
+                              },
+                        );
+      
+                        if(err) {
+                              console.log(err);
+                              return res.status(500).json({
+                                    success: false,
+                                    message: "Error"
+                              })
+                        }
+      
+                        res.status(200).json({
+                              success: true,
+                              message: "Success",
+                              data_profile: result,
+                              data: adminUpdate
+                        });
+      
+                  });
+      
+            } else {
+      
                   const adminUpdate = await AdminSchema.Admin.findOneAndUpdate(
                         {_id: req.params.id},
                         {
@@ -112,64 +175,41 @@ router.put('/:id', upload.single('foto_profile'), async (req,res) => {
                                     jenis_kelamin,
                                     alamat,
                                     no_telp,
-                                    foto_profile: result.url,
+                                    foto_profile: null,
                                     status_online,
                                     updated_at: new Date()
                               }
                         },
                   );
-
-                  if(err) {
-                        console.log(err);
-                        return res.status(500).json({
-                              success: false,
-                              message: "Error"
-                        })
-                  }
-
+      
                   res.status(200).json({
                         success: true,
                         message: "Success",
-                        data_profile: result,
                         data: adminUpdate
                   });
+      
+            }
 
+      } catch (error) {
+            res.status(500).json({
+                  success: false,
+                  message: error.message
             });
-
-      } else {
-
-            const adminUpdate = await AdminSchema.Admin.findOneAndUpdate(
-                  {_id: req.params.id},
-                  {
-                        $set : {
-                              username,
-                              password,
-                              email,
-                              nama_lengkap,
-                              tanggal_lahir,
-                              jenis_kelamin,
-                              alamat,
-                              no_telp,
-                              foto_profile: null,
-                              status_online,
-                              updated_at: new Date()
-                        }
-                  },
-            );
-
-            res.status(200).json({
-                  success: true,
-                  message: "Success",
-                  data: adminUpdate
-            });
-
       }
 
 });
 
 router.delete('/:id', async (req, res) => {
-      const admin = await AdminSchema.Admin.findByIdAndDelete(req.params.id);
-      res.json(admin);
+      try {
+            const admin = await AdminSchema.Admin.findByIdAndDelete(req.params.id);
+            res.json(admin);
+
+      } catch (error) {
+            res.status(500).json({
+                  success: false,
+                  message: error.message
+            });
+      }
 });
 
 module.exports = router;
