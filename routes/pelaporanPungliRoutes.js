7e53a536130cd37ecf272d1c77213a4a7db39bf9
Adriansyah Ravindra <adrimedia.indonesia@gmail.com>
2024-04-03 13:14:59 +0700

    4.4 Pelaporan Pungli Routes Add All Try Catch, Await POST

--- routes/pelaporanPungliRoutes.js
+++ routes/pelaporanPungliRoutes.js
@@ -16,20 +16,68 @@ router.use((req, res, next) => {
 });
 
 router.get('/', async (req, res) => {
-      const pelaporanPungli = await PelaporanPungliSchema.PelaporanPungli.find().populate('userId').populate('kategoriPungliId');
-      res.json(pelaporanPungli);
+      try {
+            const pelaporanPungli = await PelaporanPungliSchema.PelaporanPungli.find().populate('userId').populate('kategoriPungliId');
+            res.json(pelaporanPungli);
+
+      } catch (error) {
+            res.status(500).json({
+                  success: false,
+                  message: error.message
+            });
+      }
 });
 
 router.get('/:id', async (req, res) => {
-      const pelaporanPungli = await PelaporanPungliSchema.PelaporanPungli.findById(req.params.id).populate('userId').populate('kategoriPungliId');
-      res.json(pelaporanPungli);
+      try {
+            const pelaporanPungli = await PelaporanPungliSchema.PelaporanPungli.findById(req.params.id).populate('userId').populate('kategoriPungliId');
+            res.json(pelaporanPungli);
+
+      } catch (error) {
+            res.status(500).json({
+                  success: false,
+                  message: error.message
+            });
+      }
 });
 
 router.post('/', upload.single('bukti_pendukung'), async (req, res) => {
-      const { userId ,kategoriPungliId, judul_pelaporan, deskripsi_pelaporan, status_pelaporan, bukti_pendukung } = req.body;
+      try {
+            const { userId ,kategoriPungliId, judul_pelaporan, deskripsi_pelaporan, status_pelaporan, bukti_pendukung } = req.body;
 
-      if (req.file) {
-            cloudinary.uploader.upload(req.file.path, async (err, result) => {
+            if (req.file) {
+                  cloudinary.uploader.upload(req.file.path, async (err, result) => {
+                        const pelaporanPungliData = await new PelaporanPungliSchema.PelaporanPungli({
+                              userId,
+                              kategoriPungliId,
+                              judul_pelaporan,
+                              deskripsi_pelaporan,
+                              tanggal_pelaporan: new Date(),
+                              status_pelaporan,
+                              bukti_pendukung: result.url,
+                              created_at: new Date(),
+                              updated_at: new Date()
+                        });
+                  
+                        const data_keberhasilan_pelaporan_pungli = await pelaporanPungliData.save();
+      
+                        if(err) {
+                              console.log(err);
+                              return res.status(500).json({
+                                    success: false,
+                                    message: "Error"
+                              })
+                        }
+            
+                        res.status(200).json({
+                              success: true,
+                              message: "Success",
+                              data_profile: result,
+                              data: data_keberhasilan_pelaporan_pungli
+                        });
+                  });
+
+            } else {
                   const pelaporanPungliData = await new PelaporanPungliSchema.PelaporanPungli({
                         userId,
                         kategoriPungliId,
@@ -37,57 +85,69 @@ router.post('/', upload.single('bukti_pendukung'), async (req, res) => {
                         deskripsi_pelaporan,
                         tanggal_pelaporan: new Date(),
                         status_pelaporan,
-                        bukti_pendukung: result.url,
+                        bukti_pendukung: null,
                         created_at: new Date(),
                         updated_at: new Date()
                   });
-            
-                  pelaporanPungliData.save();
-
-                  if(err) {
-                        console.log(err);
-                        return res.status(500).json({
-                              success: false,
-                              message: "Error"
-                        })
-                  }
+      
+                  const data_keberhasilan_pelaporan_pungli = await pelaporanPungliData.save();
       
                   res.status(200).json({
                         success: true,
                         message: "Success",
-                        data_profile: result,
-                        data: pelaporanPungliData
+                        data: data_keberhasilan_pelaporan_pungli
                   });
-            });
-      } else {
-            const pelaporanPungliData = await new PelaporanPungliSchema.PelaporanPungli({
-                  userId,
-                  kategoriPungliId,
-                  judul_pelaporan,
-                  deskripsi_pelaporan,
-                  tanggal_pelaporan: new Date(),
-                  status_pelaporan,
-                  bukti_pendukung: null,
-                  created_at: new Date(),
-                  updated_at: new Date()
-            });
+            }
 
-            pelaporanPungliData.save();
-
-            res.status(200).json({
-                  success: true,
-                  message: "Success",
-                  data: pelaporanPungliData
+      } catch (error) {
+            res.status(500).json({
+                  success: false,
+                  message: error.message
             });
       }
 });
 
 router.put('/:id', upload.single('bukti_pendukung'), async (req,res) => {
-      const { userId ,kategoriPungliId, judul_pelaporan, deskripsi_pelaporan, tanggal_pelaporan, status_pelaporan, bukti_pendukung } = req.body;
+      try {
+            const { userId ,kategoriPungliId, judul_pelaporan, deskripsi_pelaporan, tanggal_pelaporan, status_pelaporan, bukti_pendukung } = req.body;
 
-      if (req.file) {
-
-            cloudinary.uploader.upload(req.file.path, async (err, result) => {
+            if (req.file) {
+      
+                  cloudinary.uploader.upload(req.file.path, async (err, result) => {
+                        const pelaporanPungliUpdate = await PelaporanPungliSchema.PelaporanPungli.findOneAndUpdate(
+                              {_id: req.params.id},
+                              {
+                                    $set : {
+                                          userId,
+                                          kategoriPungliId,
+                                          judul_pelaporan,
+                                          deskripsi_pelaporan,
+                                          tanggal_pelaporan: tanggal_pelaporan,
+                                          status_pelaporan,
+                                          bukti_pendukung: result.url,
+                                          updated_at: new Date()
+                                    }
+                              },
+                        );
+      
+                        if(err) {
+                              console.log(err);
+                              return res.status(500).json({
+                                    success: false,
+                                    message: "Error"
+                              })
+                        }
+            
+                        res.status(200).json({
+                              success: true,
+                              message: "Success",
+                              data_profile: result,
+                              data: pelaporanPungliUpdate
+                        });
+      
+                  });
+      
+            } else {
                   const pelaporanPungliUpdate = await PelaporanPungliSchema.PelaporanPungli.findOneAndUpdate(
                         {_id: req.params.id},
                         {
@@ -98,58 +158,39 @@ router.put('/:id', upload.single('bukti_pendukung'), async (req,res) => {
                                     deskripsi_pelaporan,
                                     tanggal_pelaporan: tanggal_pelaporan,
                                     status_pelaporan,
-                                    bukti_pendukung: result.url,
+                                    bukti_pendukung: null,
                                     updated_at: new Date()
                               }
                         },
                   );
-
-                  if(err) {
-                        console.log(err);
-                        return res.status(500).json({
-                              success: false,
-                              message: "Error"
-                        })
-                  }
       
                   res.status(200).json({
                         success: true,
                         message: "Success",
-                        data_profile: result,
                         data: pelaporanPungliUpdate
                   });
+            }
 
-            });
-
-      } else {
-            const pelaporanPungliUpdate = await PelaporanPungliSchema.PelaporanPungli.findOneAndUpdate(
-                  {_id: req.params.id},
-                  {
-                        $set : {
-                              userId,
-                              kategoriPungliId,
-                              judul_pelaporan,
-                              deskripsi_pelaporan,
-                              tanggal_pelaporan: tanggal_pelaporan,
-                              status_pelaporan,
-                              bukti_pendukung: null,
-                              updated_at: new Date()
-                        }
-                  },
-            );
-
-            res.status(200).json({
-                  success: true,
-                  message: "Success",
-                  data: pelaporanPungliUpdate
+      } catch (error) {
+            res.status(500).json({
+                  success: false,
+                  message: error.message
             });
       }
 
 });
 
 router.delete('/:id', async (req, res) => {
-      const pelaporanPungli = await PelaporanPungliSchema.PelaporanPungli.findByIdAndDelete(req.params.id);
-      res.json(pelaporanPungli);
+      try {
+            const pelaporanPungli = await PelaporanPungliSchema.PelaporanPungli.findByIdAndDelete(req.params.id);
+            res.json(pelaporanPungli);
+
+      } catch (error) {
+            res.status(500).json({
+                  success: false,
+                  message: error.message
+            });
+      }
 });
 
 module.exports = router;
