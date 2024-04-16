66b83892bda812c0d72fc05c3cbdc1777fa11051
Adriansyah Ravindra <adrimedia.indonesia@gmail.com>
2024-04-16 16:27:12 +0700

    6.2 Remove CORS Header Set Manual

--- index.js
+++ index.js
@@ -45,13 +45,6 @@ app.use(session({
       saveUninitialized: false
 }));
 
-app.use((req, res, next) => {
-      res.setHeader('Access-Control-Allow-Origin', '*');
-      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
-      res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-      next();
-});
-
 // Auth Middleware
 const authenticateToken = (req, res, next) => {
       const authHeader = req.headers['authorization'];
