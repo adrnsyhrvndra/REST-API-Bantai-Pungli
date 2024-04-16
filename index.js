dfa359eb05d53cd6f93f164522f616527f085f8c
Adriansyah Ravindra <adrimedia.indonesia@gmail.com>
2024-04-16 16:17:22 +0700

    6.1 CORS Setting Credentials

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ app.use(cors({
       origin: '*',
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
       allowedHeaders: ['Content-Type', 'Authorization'],
+      credentials: true
 }));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
