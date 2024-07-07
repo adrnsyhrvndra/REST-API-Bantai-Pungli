070131993a924e55ec00524c9507439ffb5280a7
Adriansyah Ravindra <59479003+adrnsyhrvndra@users.noreply.github.com>
2024-07-07 19:52:50 +0700

    8.2 Pelaporan pungli 2 sub schema

--- models/pelaporanPungliSchema.js
+++ models/pelaporanPungliSchema.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
 // Connect MongoDB
 // Connect MongoDB
@@ -8,48 +9,69 @@ mongoose.connect(`mongodb+srv://adrimediawebdevindonesia:ynrt!e_WyC3F_vv@bantaip
       console.log('MongoDB connection error: ' + err);
 });
 
-const pelaporanPungliSchema = new mongoose.Schema({
-      userId: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Users',
-      },
+const baseOptions = {
+      discriminatorKey: 'kind', // our discriminator key
+      collection: 'pelaporanPungli', // collection name
+};
+
+const pelaporanPungliSchema = new Schema({
       kategoriPungliId: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'KategoriPungli',
+        type: Schema.Types.ObjectId,
+        ref: 'KategoriPungli',
       },
       judul_pelaporan: {
-            type: String,
-            required: [true, 'Judul Pelaporan is required'],
-            max: [165, 'Judul Pelaporan must be less than 100 characters'],
+        type: String,
+        required: [true, 'Judul Pelaporan is required'],
+        max: [165, 'Judul Pelaporan must be less than 100 characters'],
       },
       deskripsi_pelaporan: {
-            type: String,
-            required: [true, 'Deskripsi Pelaporan is required'],
-            max: [100, 'Deskripsi Pelaporan must be less than 100 characters'],
+        type: String,
+        required: [true, 'Deskripsi Pelaporan is required'],
+        max: [100, 'Deskripsi Pelaporan must be less than 100 characters'],
       },
       tanggal_pelaporan: {
-            type: Date,
-            required: [true, 'Tangal Lahir is required'],
+        type: Date,
+        required: [true, 'Tanggal Pelaporan is required'],
       },
       status_pelaporan: {
-            type: String,
-            required: [true, 'Status Pelaporan is required'],
-            enum: ['Belum Selesai', 'Selesai', 'Dalam Proses'],
+        type: String,
+        required: [true, 'Status Pelaporan is required'],
+        enum: ['Belum Selesai', 'Selesai', 'Dalam Proses'],
       },
       bukti_pendukung: {
-            type: String,
+        type: String,
       },
       created_at: {
-            type: Date,
-            default: Date.now
+        type: Date,
+        default: Date.now,
       },
       updated_at: {
-            type: Date,
-            default: Date.now
+        type: Date,
+        default: Date.now,
       },
-});
+}, baseOptions);
 
+// Model utama
 const PelaporanPungli = mongoose.model('PelaporanPungli', pelaporanPungliSchema);
+
+// Sub-skema untuk Users
+const PelaporanPungliUsers = PelaporanPungli.discriminator('Users', new Schema({
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'Users',
+  },
+}));
+
+// Sub-skema untuk Admin
+const PelaporanPungliAdmin = PelaporanPungli.discriminator('Admin', new Schema({
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'Admin',
+  },
+}));
+
 module.exports = {
-      PelaporanPungli
+      PelaporanPungli,
+      PelaporanPungliUsers,
+      PelaporanPungliAdmin
 };
